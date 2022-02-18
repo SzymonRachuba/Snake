03b19d214700259915b0289165e4355ba62e97be
Szymon Rachuba <szympczor@gmail.com>
2022-02-18 22:39:47 +0100

    Working on Snake

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include "Board.h"
 
 using namespace std;
 
+
 int main() {
 
-	cout << "Hello World!" << endl;
 
-	system("pause");
+	Board board = Board();
+
 	return 0;
 }
